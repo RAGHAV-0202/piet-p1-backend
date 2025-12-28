c68e0a6d25ea2a9c4d03ac4d67c348089d75fcff
Raghav Kapoor <101061750+RAGHAV-0202@users.noreply.github.com>
2025-12-28 22:23:39 +0530

    Add password reset routes to auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {UserLogin , UserRegister , UserLogout , IsLoggedIn} from "../controllers/auth.controller.js"
+import {UserLogin , UserRegister , UserLogout , IsLoggedIn , UserPasswordResetRequest  , UserPasswordResetPage} from "../controllers/auth.controller.js"
 
 const Router = express.Router()
 
@@ -7,5 +7,8 @@ Router.route("/login").post(UserLogin)
 Router.route("/register").post(UserRegister)
 Router.route("/logout").post(UserLogout)
 Router.route("/loggedin").get(IsLoggedIn)
+Router.route("/reset").post(UserPasswordResetRequest)
+Router.route("/reset/:token").post(UserPasswordResetPage)
+
 
 export default Router
